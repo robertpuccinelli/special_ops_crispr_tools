96ae65d8ecbf502cb65f4c8a2b50cc70962f537b
David <david@block-party.net>
2018-04-03 12:18:37 -0700

    split out some common FLASH/DASHit code to ASH (#7)
    Move some common FLASH/DASH code to a new repository 'ASH'

---
+++ ash/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ash',
+    version='1.0',
+    description='Shared code for (FL|D)ash.',
+    author='David Dynerman',
+    author_email='david.dynerman@czbiohub.org',
+    packages=['ash'],
+)
